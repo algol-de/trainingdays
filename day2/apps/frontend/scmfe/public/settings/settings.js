d0ac9b6b7d383b18444d936bb8643bdb19921f3f
Karsten Walter <kw@algol.de>
2022-05-06 10:56:28 +0200

    changed org name in common workflow

--- day2/apps/frontend/scmfe/public/settings/settings.js
+++ day2/apps/frontend/scmfe/public/settings/settings.js
@@ -1,5 +1,5 @@
 var uisettings = {
-    "endpoint": "",
-    "resourcesEndpoint": "",
-    "aiKey": ""
+    "endpoint": "https://kw-scm-breakout.azurewebsites.net/",
+    "resourcesEndpoint": "https://kwscmresourcesapi.azurewebsites.net/",
+    "aiKey": "?sv=2020-08-04&ss=bfqt&srt=o&sp=rtfx&se=2022-05-03T23:40:16Z&st=2022-05-03T15:40:16Z&spr=https&sig=aCn2r6LjNuDNlXiL%2BUjB1omZQEYBb4kHecrPlixZaNM%3D"
 }
